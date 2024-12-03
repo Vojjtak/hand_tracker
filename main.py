448312cbebffde5349a531ad6baa5f1e46bd3fdd
Vojjtak <vodos@seznam.cz>
2024-12-03 13:35:14 +0100

    functional - need to be uploaded trained datasets. Only 1,2,3

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
+from cvzone.ClassificationModule import Classifier
 import numpy as np
 import math
+import time
+import tensorflow
+
+
 
 cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands=1)
+classifier = Classifier("model/keras_model.h5", "model/labels.txt")
 
 offset = 20
 imgSize = 300
 
+folder = "Data/C"
+counter = 0
+
+labels = ["1", "2", "3"]
+
+
+
 while True:
     succes, img = cap.read()
+    imgOutput = img.copy()
     hands, img = detector.findHands(img)
     if hands:
         hand = hands[0]
@@ -32,6 +46,8 @@ while True:
             imgResizeShape = imgResize.shape
             wGap = math.ceil((imgSize-wCal)/2)
             imgWhite[:, wGap:wCal+wGap] = imgResize
+            prediction, index = classifier.getPrediction(imgWhite, draw=False)
+            print(prediction, index)
 
         else:
             k = imgSize / w
@@ -39,10 +55,18 @@ while True:
             imgResize = cv2.resize(imgCrop, (imgSize, hCal))
             imgResizeShape = imgResize.shape
             hGap = math.ceil((imgSize - hCal) / 2)
-            imgWhite[hGap:hCal + hGap, : ] = imgResize
+            imgWhite[hGap:hCal + hGap, :] = imgResize
+            prediction, index = classifier.getPrediction(imgWhite, draw=False)
+
+        cv2.putText(imgOutput, labels[index], (x,y - 20), cv2.FONT_HERSHEY_COMPLEX, 2, (225,0,225), 2)
+        cv2.rectangle(imgOutput, (x-offset,y-offset), (x+w+offset,y+h-offset),(225,0,225), 4)
+
 
         cv2.imshow("ImageCrop", imgCrop)
         cv2.imshow("ImageCrop", imgWhite)
 
-    cv2.imshow("Image", img)
+    cv2.imshow("Image", imgOutput)
     cv2.waitKey(1)
+
+
+
